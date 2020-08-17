ab7642be24fc57f58323ab56dae4742d9e65c54f
Mike <cyberenot@gmail.com>
2020-08-17 17:12:21 -0400

    Lecture 7: Multithreading EmojiArt

---
+++ EmojiArt/EmojiArt/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Mike on 2020-08-15.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
